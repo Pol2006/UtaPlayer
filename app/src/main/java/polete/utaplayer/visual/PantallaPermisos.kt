694b190c2033481623c65e3f4356440bce6b3178
Polete <pbusquets-dam@pbusquets.dam@institutcampalans.net>
2026-04-30 20:45:48 +0200

    afegit tip

--- app/src/main/java/polete/utaplayer/visual/PantallaPermisos.kt
+++ app/src/main/java/polete/utaplayer/visual/PantallaPermisos.kt
@@ -29,12 +29,13 @@ fun PantallaPermisos(onGrantClick: () -> Unit) {
             Text("Benvingut a", style = MaterialTheme.typography.titleLarge, fontSize = 52.sp, color = MaterialTheme.colorScheme.primary)
             Text("UTA Player", style = MaterialTheme.typography.titleLarge, fontSize = 52.sp, color = MaterialTheme.colorScheme.primary)
             Spacer(Modifier.padding(top = 56.dp))
-            Text("Tip: Recorda tenir la teva musica guardada a la carpeta per defecte del dispositiu ( /Music )", style = MaterialTheme.typography.bodyMedium, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
+            Text("Tip: Recorda tenir la teva musica guardada a  la carpeta per defecte del dispositiu ( /Music )", style = MaterialTheme.typography.bodyMedium, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
+            Spacer(Modifier.padding(top = 56.dp))
+            Text("Si vols gaudir al màxim de l'experiència, intenta que les cançons tinguin les metadades", style = MaterialTheme.typography.bodyMedium, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
 
             Box(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
                         Button(onClick = onGrantClick, Modifier.align(Alignment.Center)) { Text("Donar permís per veure la música", style = MaterialTheme.typography.bodyMedium, fontWeight = FontWeight.Bold) }
                     }
-
             }
 
         }
